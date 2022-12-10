7130a5029abb49e0c3134acb1a3a8a236d49e3b9
Young Shung <me@ynshung.com>
2022-12-10 21:15:02 +0800

    rm unnecessary file

--- src/php/list_csv_file.php
+++
@@ -1,7 +0,0 @@
-<?php
-$path    = '../csv';
-
-// Scan the directory for files and skip the current and parent directories
-$files = array_diff(scandir($path), array('.', '..'));
-
-echo json_encode(array_values($files));
--- src/php/list_excel_file.php
+++
@@ -1,7 +0,0 @@
-<?php
-$path    = '../excel';
-
-// Scan the directory for files and skip the current and parent directories
-$files = array_diff(scandir($path), array('.', '..'));
-
-echo json_encode(array_values($files));
